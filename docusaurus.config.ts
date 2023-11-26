595ff7798e6e6203b2d4dd0f6333ffb172301860
Phil Skaggs <16527164+artnoisenik@users.noreply.github.com>
2023-11-26 13:48:01 -0700

    Random Update.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,7 +79,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} Machinelee. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Machinelee Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
